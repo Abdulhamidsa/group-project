84001963cdf6c9038551064faf2c5b8f468849a2
Marta <m.nowak033@gmail.com>
2022-11-21 14:47:06 +0100

    trying to get input from form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import {useRef} from 'react';
 
 function Form() {
-    const SoMe = [{name: "Tik Tok", co2: 2.63}, {name: "Reddit", co2: 2.48},{name: "Pinterest", co2: 1.30}, {name: "Instagram", co2: 1.05}, {name: "Snapchat", co2: 0.87}, {name: "Facebook", co2: 0.79}, {name: "LinkedIn", co2: 0.71}, {name: "Twitter", co2: 0.60}, {name: "Twitch", co2: 0.55}, {name: "Youtube", co2: 0.46}]
+    const formInputs = useRef();
+    // array with SoMe
+    const SoMe = [{name: "Tik Tok", key: "tiktok", co2: 2.63}, {name: "Reddit", key: "reddit" , co2: 2.48},{name: "Pinterest", key: "pinterest", co2: 1.30}, {name: "Instagram", key: "ig", co2: 1.05}, {name: "Snapchat", key: "snapchat", co2: 0.87}, {name: "Facebook", key: "fb", co2: 0.79}, {name: "LinkedIn", key: "li", co2: 0.71}, {name: "Twitter", key: "twitter", co2: 0.60}, {name: "Twitch", key: "twitch", co2: 0.55}, {name: "Youtube", key: "yt", co2: 0.46}]
+
+    // function to calculate co2 produced
+
+    function calculateFootprint(event) {
+        event.preventDefault();
+        console.log(formInputs);
+    }
+
   return (
     <>
-        <form>
+        <form onSubmit={calculateFootprint} ref={formInputs}>
             {SoMe.map((element) => {
                 return(
-                    <div className='form-control'>
-                        <label for={element.name}>{element.name}</label>
-                        <input type='number'></input>
+                    <div className='form-control' key={element.key}>
+                        <label htmlFor={element.name}>{element.name}</label>
+                        <input type='number' name={element.key} co2={element.co2}></input>
                     </div>
                 )
             })}
-            
+            <button>Calculate</button>
         </form>
     </>
   )
