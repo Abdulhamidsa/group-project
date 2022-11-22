5cd2a01d1e765356f07bafde67f12f13be1295e2
Aboood <abdu3793@stud.kea.dk>
2022-11-22 07:23:27 +0100

    add new object

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -18,18 +18,15 @@ function Form() {
     { name: "Youtube", key: "yt", co2: 0.46 },
   ];
 
-  const [details, setDetails] = useState([
-    { name: "Tik Tok", key: "tiktok", co2: 2.63 },
-    { name: "Reddit", key: "reddit", co2: 2.48 },
-    { name: "Pinterest", key: "pinterest", co2: 1.3 },
-    { name: "Instagram", key: "ig", co2: 1.05 },
-    { name: "Snapchat", key: "snapchat", co2: 0.87 },
-    { name: "Facebook", key: "fb", co2: 0.79 },
-    { name: "LinkedIn", key: "li", co2: 0.71 },
-    { name: "Twitter", key: "twitter", co2: 0.6 },
-    { name: "Twitch", key: "twitch", co2: 0.55 },
-    { name: "Youtube", key: "yt", co2: 0.46 },
-  ]);
+  const [details, setDetails] = useState({
+    ig: "",
+    snapchat: "",
+    fb: "",
+    li: "",
+    twitter: "",
+    twitch: "",
+    yt: "",
+  });
 
   // function to calculate co2 produced
 
