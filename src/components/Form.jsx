67ac202850b7f35a2289b974b366b4a0f5790bd7
Jeppe <jepp026b@edu.zealand.dk>
2022-11-24 12:27:23 +0100

    made it ready for screen shots

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,16 +8,16 @@ function Form() {
   const [submitted, setSubmitted] = useState(false);
   // array with SoMe
   const SoMe = [
-    { name: "Tik Tok", key: "tiktok"},
-    { name: "Reddit", key: "reddit"},
-    { name: "Pinterest", key: "pinterest"},
-    { name: "Instagram", key: "ig"},
-    { name: "Snapchat", key: "snapchat"},
-    { name: "Facebook", key: "fb"},
-    { name: "LinkedIn", key: "li"},
-    { name: "Twitter", key: "twitter"},
-    { name: "Twitch", key: "twitch"},
-    { name: "Youtube", key: "yt"},
+    { name: "Tik Tok", key: "tiktok" },
+    { name: "Reddit", key: "reddit" },
+    { name: "Pinterest", key: "pinterest" },
+    { name: "Instagram", key: "ig" },
+    { name: "Snapchat", key: "snapchat" },
+    { name: "Facebook", key: "fb" },
+    { name: "LinkedIn", key: "li" },
+    { name: "Twitter", key: "twitter" },
+    { name: "Twitch", key: "twitch" },
+    { name: "Youtube", key: "yt" },
   ];
 
   const multiply = {
@@ -34,49 +34,45 @@ function Form() {
 
   // function to calculate co2 produced
 
+  //Some nice code
+  // const values = keys.map((entries) => {
+  //   const obj = {};
+  //   obj[entries] = formInputs.current.elements[entries].value * multiply[entries] * 60;
+  //   return { ...obj };
+  // });
+
   const handleSubmit = (event) => {
     console.log("handleSubmit ran");
     console.log("data", formInputs.current.elements);
     event.preventDefault(); // 👈️ prevent page refresh
     const keys = Object.keys(multiply);
-    // const values = keys.map((entries) => {
-    //   const obj = {};
-    //   obj[entries] = formInputs.current.elements[entries].value * multiply[entries] * 60;
-    //   return { ...obj };
-    // });
     let values = {};
     keys.map((entries) => {
       values[entries] = formInputs.current.elements[entries].value * multiply[entries] * 60;
     });
     console.log(values);
-    
-    // 👇️ access input values here
-    // const formObj = {};
-    // formObj.tiktok = formInputs.current.elements.tiktok.value * multiply.tiktok;
-    // console.log("first 👉️", formInputs.current.elements.tiktok.value * multiply.tiktok);
-    // console.log("last 👉️", formInputs.current.elements.reddit.value * multiply.reddit);
-
     setFormData(values);
-    // 👇️ clear all input values in the form
     setSubmitted(true);
     return values;
   };
 
   return (
     <>
-        {submitted ? <Calculate some={SoMe} results={formData} /> : (
-            <form onSubmit={handleSubmit} ref={formInputs}>
-              {SoMe.map((element) => {
-                return (
-                  <div className="form-control" key={element.key}>
-                    <label htmlFor={element.name}>{element.name}</label>
-                    <input type="number" name={element.key}></input>
-                  </div>
-                );
-              })}
-              <button>Calculate</button>
-            </form>
-        )}
+      {submitted ? (
+        <Calculate some={SoMe} results={formData} />
+      ) : (
+        <form onSubmit={handleSubmit} ref={formInputs}>
+          {SoMe.map((element) => {
+            return (
+              <div className="form-control" key={element.key}>
+                <label htmlFor={element.name}>{element.name}</label>
+                <input type="number" name={element.key}></input>
+              </div>
+            );
+          })}
+          <button>Calculate</button>
+        </form>
+      )}
     </>
   );
 }
