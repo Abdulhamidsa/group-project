0403fabadd38a0153c7ae91b71ef1c2e0c489526
Aboood <abdu3793@stud.kea.dk>
2022-11-24 20:42:07 +0100

    add button to go step back

--- src/components/Calculate.jsx
+++ src/components/Calculate.jsx
@@ -1,5 +1,7 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Form from "./Form";
 export default function Calculate(props) {
+  const [start, setStart] = useState(false);
   const scollToRef = useRef();
   const handleClick = (ev) => {
     scollToRef.current.scrollIntoView({ behavior: "smooth" });
@@ -15,35 +17,44 @@ export default function Calculate(props) {
   }
   return (
     <>
-      <section className="result">
-        <p>YOUR TOTAL IS : </p>
-        <h2>
-          {sumUp(resultsObject)}g of CO2 <span className="span-underline"> PER DAY !</span>
-        </h2>
-        <p>
-          It is as if you manufactured <span className="span-clr"> {calculateBottles()}</span> plastic bottles every day.
-        </p>
-        <p>
-          In one year, you would produce <span className="span-clr"> {calculateBottles() * 365}</span> plastic bottles.
-        </p>
-        <button onClick={() => handleClick()}>DETALED RESULTS</button>
-      </section>
-      <section ref={scollToRef} className="detailed-result">
-        <div>
-          <h2>Detailed results</h2>
-          <p>Check which social media have the highest carbon footprint.</p>
-        </div>
-        <div className="details-cont">
-          {props.some.map((element) => {
-            return (
-              <div className="some-result">
-                <p className="result-nr"> {element.name}</p>
-                <p>{Math.trunc(props.results[element.key])}g co2.</p>
-              </div>
-            );
-          })}
-        </div>
-      </section>
+      {start ? (
+        <Form />
+      ) : (
+        <>
+          <section className="result">
+            <p>YOUR TOTAL IS : </p>
+            <h2>
+              {sumUp(resultsObject)}g of CO2 <span className="span-underline"> PER DAY !</span>
+            </h2>
+            <p>
+              It is as if you manufactured <span className="span-clr"> {calculateBottles()}</span> plastic bottles every day.
+            </p>
+            <p>
+              In one year, you would produce <span className="span-clr"> {calculateBottles() * 365}</span> plastic bottles.
+            </p>
+            <button onClick={() => handleClick()}>DETALED RESULTS</button>
+          </section>
+          <section ref={scollToRef} className="detailed-result">
+            <div>
+              <h2>Detailed results</h2>
+              <p>Check which social media have the highest carbon footprint.</p>
+            </div>
+            <div className="details-cont">
+              {props.some.map((element) => {
+                return (
+                  <div className="some-result">
+                    <p className="result-nr"> {element.name}</p>
+                    <p>{Math.trunc(props.results[element.key])}g co2.</p>
+                  </div>
+                );
+              })}
+            </div>
+            <button onClick={() => setStart(true)} aria-label="Show the form">
+              CALCULATE AGAIN
+            </button>
+          </section>
+        </>
+      )}
     </>
   );
 }
